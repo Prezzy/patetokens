15f6c9c834bd468503e58777e785d53a9b92c380
Prezzy <pjjhaffe@ucalgary.ca>
2023-01-19 14:53:47 -0700

    trying to fix split_sk

--- NistKey.py
+++ NistKey.py
@@ -68,15 +68,15 @@ class DistributedKey(Key):
         self.group_pks = group_pks
         
     def from_json(self, json_keys):
-        super(DistributedKey, self).from_json(json_keys)
-        #self.p = utils.b64str_to_gmp(json_keys['p'])
-        #self.q = utils.b64str_to_gmp(json_keys['q'])
-        #self.g = utils.b64str_to_gmp(json_keys['g'])
-        #self.y = utils.b64str_to_gmp(json_keys['y'])
-        #self.group_pks = dict(map(lambda keyi: (keyi[0], utils.b64str_to_gmp(keyi[1])), json_keys['group-pks'],items()))
+        #super(DistributedKey, self).from_json(json_keys)
+        self.p = utils.b64str_to_gmp(json_keys['p'])
+        self.q = utils.b64str_to_gmp(json_keys['q'])
+        self.g = utils.b64str_to_gmp(json_keys['g'])
+        self.y = utils.b64str_to_gmp(json_keys['y'])
+        self.group_pks = dict(map(lambda keyi: (keyi[0], utils.b64str_to_gmp(keyi[1])), json_keys['group-pks'],items()))
         self.x_share = json_keys['x-shares']
-        #if 'x' in json_keys.keys():
-        #    self.x = utils.b64str_to_gmp(json_keys['x'])
+        if 'x' in json_keys.keys():
+            self.x = utils.b64str_to_gmp(json_keys['x'])
 
     def export(self, Public=True):
         json_keys = super(DistributedKey, self).export_keys(Public)
