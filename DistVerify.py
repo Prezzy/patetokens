ea13160b910bd6ae81c16f784b875959d5977d22
Prezzy <pjjhaffe@ucalgary.ca>
2023-01-19 19:43:01 -0700

    updating NistKey

--- DistVerify.py
+++ DistVerify.py
@@ -49,14 +49,10 @@ def round2(i, nonces, B, V, step1_responses, key):
 
     coeffs = shamir.lagrange_coefficients(IntGMP(0), indexs_gmp, key.q)
 
-    print("+++++")
-    print(coeffs[int(i)])
-    print("+++++")
-    print(key.x_share)
     a_i = coeffs[int(i)].__mul__(key.x_share)
     a_i.inplace_pow(1, key.q)
     C_bar = yg.b.__pow__(a_i,key.p)
-    zeta = utils.rand_feild_element(key)
+    zeta = utils.rand_felement_gmp(key)
     R_i = Cipher.Cipher(key=key)
     R_i.encrypt(zeta, a_i)
     C = dict()
